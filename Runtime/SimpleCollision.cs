d709e953ce25522cffcedf5f3724e2a9c5091a8d
Castle <castle.wsc@gmail,com>
2021-08-04 18:06:23 +0800

    update to 1.0.1
    add SimpleCollision

---
+++ Runtime/SimpleCollision.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Kazegames.Utility
+{
+    [AddComponentMenu(Globals.MENU + "Simple Collision")]
+    public class SimpleCollision : MonoBehaviour
+    {
+        [SerializeField] string Tag = "";
+
+        [SerializeField] UnityEvent onEnter;
+        [SerializeField] UnityEvent onExit;
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if(string.IsNullOrEmpty(Tag))
+            {
+                onEnter?.Invoke();
+            }
+            else
+            {
+                if(collision.collider.CompareTag(Tag))
+                {
+                    onEnter?.Invoke();
+                }
+            }
+        }
+
+        private void OnCollisionExit(Collision collision)
+        {
+            if (string.IsNullOrEmpty(Tag))
+            {
+                onExit?.Invoke();
+            }
+            else
+            {
+                if (collision.collider.CompareTag(Tag))
+                {
+                    onExit?.Invoke();
+                }
+            }
+        }
+
+    }
+}
