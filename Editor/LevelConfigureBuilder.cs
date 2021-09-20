d80917cea4871670d43faa1e09f6f7ddc82cf1a2
Castle <castle.wsc@gmail,com>
2021-09-20 14:18:23 +0800

    update to 1.0.4

--- Editor/LevelConfigureBuilder.cs
+++ Editor/LevelConfigureBuilder.cs
@@ -7,15 +7,83 @@ using System.Collections.Generic;
 
 namespace Kazegames.Editor
 {
-    public class LevelConfigureBuilder : IPreprocessBuildWithReport
+    [CustomEditor(typeof(LevelConfigure))]
+    public class LevelConfigureBuilder : UnityEditor.Editor, IPreprocessBuildWithReport
     {
+        SerializedProperty _spStartupScenes;
+        SerializedProperty _spLevelScenes;
+
         public int callbackOrder { get { return 0; } }
 
-        [MenuItem("Kazegames/Build Levels")]
-        private static void BuildLevels()
+        void OnEnable()
         {
-            LevelConfigure config = Resources.Load<LevelConfigure>("Level Configure");
+            _spStartupScenes = serializedObject.FindProperty("startupScenes");
+            _spLevelScenes = serializedObject.FindProperty("levelScenes");
+        }
 
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            EditorGUILayout.PropertyField(_spStartupScenes);
+            EditorGUILayout.PropertyField(_spLevelScenes);
+
+            serializedObject.ApplyModifiedProperties();
+
+            EditorGUILayout.Space(20);
+
+            if(GUILayout.Button("Build Levels"))
+            {
+                LevelConfigure config = Resources.Load<LevelConfigure>("Level Configure");
+
+                if (config == null)
+                {
+                    if (EditorUtility.DisplayDialog("Error: Not found the LevelConfigure file", "You should create a LevelConfigure file named \"Level Configure\" in UNITY Resources folder", "Cancel"))
+                    {
+                        return;
+                    }
+                }
+
+                BuildLevels(config);
+            }
+        }
+
+        private void BuildLevels(LevelConfigure config)
+        {
+            // change building scenes
+            Debug.LogWarning("change building scenes..");
+
+            List<EditorBuildSettingsScene> buildingList = new List<EditorBuildSettingsScene>();
+
+            if (config.startupScenes != null && config.startupScenes.Length > 0)
+            {
+                foreach (var s in config.startupScenes)
+                {
+                    string path = AssetDatabase.GetAssetPath(s);
+                    string guid = AssetDatabase.AssetPathToGUID(path);
+
+                    buildingList.Add(new EditorBuildSettingsScene(guid, true));
+
+                    Debug.Log($"added scene({s.name}): {path}");
+                }
+            }
+
+            if (config.levelScenes != null && config.levelScenes.Length > 0)
+            {
+                foreach (var s in config.levelScenes)
+                {
+                    string path = AssetDatabase.GetAssetPath(s);
+                    string guid = AssetDatabase.AssetPathToGUID(path);
+
+                    buildingList.Add(new EditorBuildSettingsScene(guid, true));
+
+                    Debug.Log($"added scene({s.name}): {path}");
+                }
+            }
+
+            EditorBuildSettings.scenes = buildingList.ToArray();
+
+            // build level configure
             config.Build();
         }
 
@@ -25,46 +93,12 @@ namespace Kazegames.Editor
             {
                 LevelConfigure config = Resources.Load<LevelConfigure>("Level Configure");
 
-                if(config == null)
+                if (config == null)
                 {
                     throw new System.NullReferenceException("Not found the LevelConfigure file, you should create a LevelConfigure file named \"Level Configure\" in UNITY Resources folder");
                 }
 
-                // change building scenes
-                Debug.Log("change building scenes..");
-
-                List<EditorBuildSettingsScene> buildingList = new List<EditorBuildSettingsScene>();
-
-                if(config.startupScenes != null && config.startupScenes.Length > 0)
-                {
-                    foreach(var s in config.startupScenes)
-                    {
-                        string path = AssetDatabase.GetAssetPath(s);
-                        string guid = AssetDatabase.AssetPathToGUID(path);
-
-                        buildingList.Add(new EditorBuildSettingsScene(guid, true));
-
-                        Debug.Log($"added scene({s.name}): {path}");
-                    }
-                }
-
-                if(config.levelScenes != null && config.levelScenes.Length > 0)
-                {
-                    foreach (var s in config.levelScenes)
-                    {
-                        string path = AssetDatabase.GetAssetPath(s);
-                        string guid = AssetDatabase.AssetPathToGUID(path);
-
-                        buildingList.Add(new EditorBuildSettingsScene(guid, true));
-
-                        Debug.Log($"added scene({s.name}): {path}");
-                    }
-                }
-
-                EditorBuildSettings.scenes = buildingList.ToArray();
-
-                // build level configure
-                config.Build();
+                BuildLevels(config);
             }
         }
     }
